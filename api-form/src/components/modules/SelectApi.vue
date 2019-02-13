f4df41250192d5264cf45645be4be1653a4b21c5
Kohei Matsumoto <baberugaberiimeronnv5@gmail.com>
2019-02-13 20:22:34 +0900

    computed, fetchとか

--- api-form/src/components/modules/SelectApi.vue
+++ api-form/src/components/modules/SelectApi.vue
@@ -1,13 +1,30 @@
 <template>
-    <select v-model="selected">
-        <option v-for='option in options' v-bind:value="option.value">
-            {{ option.text }}
-        </option>
-    </select>
+    <div>
+        <select v-model="selected">
+            <!-- eslint-disable-next-line -->        
+            <option v-for='option in options' v-bind:value="option.value">
+                {{ option.text }}
+            </option>
+        </select>
+        <p>{{ selected }} = {{ selected2 }}</p>
+
+        <hr>
+
+        <select v-model="selected">
+            <option v-for="api in apis" v-bind:value="api.url">
+                {{ api.name }}   |   {{ api.key }}
+            </option>
+        </select>
+
+        <hr>
+
+        <p>{{ getApiConfigs }}</p>
+    </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapState } from 'vuex'
+import store from '@/store'
 
 export default {
     name: 'SelectApi',
@@ -16,19 +33,31 @@ export default {
     },
     data() {
         return {
+            selected: store.state.select_api.selected,
             // storeにあるstateから持ってきたい
             // 最終はfetchとかでjsonから
-            selected: '',
-            options: [
-                { text: 'item1', value: 'item1' },
-                { text: 'item2', value: 'item2' },
-                { text: 'item3', value: 'item3' },
-            ]
+            options: store.state.select_api.options,
+            selected2: store.state.select_api.selected,
+            apis: store.state.select_api.apis,
+
+            /**
+             * formm全体を1componentとするのであれば
+             * とりあえずlocalでそれぞれの情報を持っていて...
+             * そもそもそれなら、全体で共有するタイミングがないな...
+             * あー,結果とか？
+             */
         }
     },
-    comptued: mapGetters({
-        'options': 'getOptions'
-    })
+    computed: {
+        getApiConfigs: function() {
+            fetch(this.selected)
+                .then(res => res.json)
+                .then(res => {
+                    console.log(res)
+                })
+            return this.selected + " | aiueo"
+        }
+    }
 }
 </script>
 
--- api-form/src/store/index.js
+++ api-form/src/store/index.js
@@ -3,9 +3,9 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-const SelectApi = {
+const select_api = {
     state: {
-        selected: '',
+        selected: 'xxxxxxxxxxx',
         options: [
             { text: 'item1', value: 'item1' },
             { text: 'item2', value: 'item2' },
@@ -19,8 +19,30 @@ const SelectApi = {
     }
 }
 
+/*
+fetch('http://localhost:3000/apis')
+    .then(res => res.json())
+    .then(res => {select_api.state.apis = res})
+*/
+
+
+/** 
+ * new Vuexの後にresponseが来ると、
+ * componentからは変更後のstateを後から取りに行かなければならない？
+ * ここは非同期じゃなくて、全部読み込んでから処理を開始したい
+ */
+fetch('http://localhost:3000/apis')
+    .then(res => res.json())
+    .then(res => {
+        console.log(res)
+        select_api.state.apis = res
+    })
+
+console.log(select_api.state)
+
 export default new Vuex.Store({
+    namespace: true,
     modules: {
-        SelectApi
-    }
+        select_api
+    },
 })
